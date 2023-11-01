655436f225d2bbe17d76131a233ef545b372fdf7
M0H1K4N <nejer911@gmail.com>
2023-11-01 22:11:52 +0400

    Added real projects

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Projects from "./Components/Projects/Projects.js";
 import Built from "./Components/Built-Projects/Built";
 import Footer from "./Components/Footer/Footer";
 
-import img1 from './images/essentials.png'
-import img2 from './images/tic-tac-toe.png'
-import img3 from './images/thumbnail-project-3-small.webp'
-
+import img1 from "./images/essentials.png";
+import img2 from "./images/tic-tac-toe.png";
+import img3 from "./images/inv-calculator-img.png";
+import img4 from "./images/REART-img.png";
+import img5 from "./images/Countdown.png";
 
 function App() {
- 
   return (
     <div className="App">
       <Foto />
@@ -25,19 +25,31 @@ function App() {
         description={"React.js"}
         link={"https://m0h1k4.github.io/React-Essentials/"}
         img={img1}
-        
       />
       <Built
-            title={ "Tic-tac-toe"}
-            description={"React + vite"}
-            link={"https://m0h1k4.github.io/React-vite--tic-tac-toe/"}
-            img={img2}
+        title={"Tic-tac-toe"}
+        description={"React + vite"}
+        link={"https://m0h1k4.github.io/React-vite--tic-tac-toe/"}
+        img={img2}
       />
       <Built
-        title= {"TODO WEB APP"}
-        description= {"HTML"}
-        link= {"https://m0h1k4.github.io/tip-calculator-project/"}
+        title={"Investment Calculator"}
+        description={"React.js Vite"}
+        link={"https://m0h1k4.github.io/Investment-calculator-Vite/"}
         img={img3}
+      />
+      <Built
+        title={"React Art"}
+        description={"React.js Vite"}
+        link={"https://m0h1k4.github.io/ReactArt/"}
+        img={img4}
+      />
+
+      <Built
+        title={"CountDown"}
+        description={"React.js Vite"}
+        link={"https://m0h1k4.github.io/Countdown/"}
+        img={img5}
       />
 
       <Footer />
